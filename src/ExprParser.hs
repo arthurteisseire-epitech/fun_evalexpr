9a3350c125cb346c4d046d2d2b9fedd9b802afde
arthur <arthur.teisseire@epitech.eu>
2019-10-28 13:08:40 +0100

    Parse float values

--- src/ExprParser.hs
+++ src/ExprParser.hs
@@ -1,14 +1,33 @@
 module ExprParser where
 
-import Expression
-import Text.ParserCombinators.ReadP
-import Data.Char
-import Debug.Trace
+import           Control.Applicative
+import           Data.Char
+import           Debug.Trace
+import           Expression
+import           Text.ParserCombinators.ReadP
+
+(<++>) a b = (++) <$> a <*> b
+
+(<:>) a b = (:) <$> a <*> b
+
+number = munch1 isDigit
+
+plus = char '+' *> number
+
+minus = char '-' <:> number
+
+integer = plus <|> minus <|> number
+
+decimal = option "" $ char '.' <:> number
 
 parseVal :: ReadP Expr
-parseVal = do
-    n <- read <$> munch1 isDigit
-    return $ Val n
+parseVal = Val . rd <$> (integer <++> decimal)
+  where
+    rd = read :: String -> Float
 
-parseExpr :: String -> Expr
-parseExpr s = fst $ head $ readP_to_S parseVal s
+parseExpr :: String -> Maybe Expr
+parseExpr s
+    | null a = Nothing
+    | otherwise = Just $ fst $ last a
+  where
+    a = readP_to_S parseVal s
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -11,7 +11,9 @@ main = hspec spec
 spec :: Spec
 spec =
     describe "simple" $ do
-        it "test pdarsing value" $
-            parseExpr "12," `shouldBe` Val 12
+        it "test parsing value error" $
+            parseExpr "fsdafsa" `shouldBe` Nothing
         it "test parsing value" $
-            parseExpr "21,2" `shouldBe` Val 21
+            parseExpr "12.0" `shouldBe` Just (Val 12)
+        it "test parsing value" $
+            parseExpr "21.2" `shouldBe` Just (Val 21.2)
