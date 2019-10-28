e0b73ed27ca5c084bdbb93d153daceb3fef66aac
arthur <arthur.teisseire@epitech.eu>
2019-10-28 14:23:01 +0100

    Rename ExprSpec into ValueSpec

--- test/ValueSpec.hs
+++ test/ValueSpec.hs
@@ -1,4 +1,4 @@
-module ParserSpec where
+module ValueSpec where
 
 
 import Test.Hspec
@@ -13,11 +13,11 @@ spec =
     describe "simple" $ do
         it "test parsing value error" $
             parseExpr "fsdafsa" `shouldBe` Nothing
-        it "test parsing value" $
+        it "test parsing integer value" $
             parseExpr "12.0" `shouldBe` Just (Val 12)
-        it "test parsing value" $
+        it "test parsing float value" $
             parseExpr "21.2" `shouldBe` Just (Val 21.2)
-        it "test parsing value" $
+        it "test parsing float value with plus" $
             parseExpr "+21.2" `shouldBe` Just (Val 21.2)
-        it "test parsing value" $
+        it "test parsing float value with minus" $
             parseExpr "-21.2" `shouldBe` Just (Val (-21.2))
