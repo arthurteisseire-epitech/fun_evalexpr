3d777efd96c0aedae733ad791c616519cfdf3f14
arthur <arthur.teisseire@epitech.eu>
2019-10-29 10:08:08 +0100

    Implement evalExpr in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
-import Lib
+import           Data.Maybe
+import           ExprParser
+import           System.Environment
+import           Text.Printf
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    output $ exec args
+
+output :: Maybe Float -> IO ()
+output f
+    | isNothing f = putStrLn "Fail"
+    | otherwise = printf "%.2f\n" (fromJust f)
+
+exec :: [String] -> Maybe Float
+exec s
+    | length s /= 1 = Nothing
+    | otherwise = evalExpr $ head s
