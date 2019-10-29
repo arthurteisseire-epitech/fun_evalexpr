08b4d86f885df7034a5371d2f9e0b5d8d1c548b6
arthur <arthur.teisseire@epitech.eu>
2019-10-29 17:30:15 +0100

    Exit 84 when fail

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import           Data.Maybe
 import           ExprParser
 import           System.Environment
+import           System.Exit
 import           Text.Printf
 
 main :: IO ()
@@ -12,10 +13,16 @@ main = do
 
 output :: Maybe Float -> IO ()
 output f
-    | isNothing f = putStrLn "Fail"
+    | isNothing f = exitWithHelp
     | otherwise = printf "%.2f\n" (fromJust f)
 
 exec :: [String] -> Maybe Float
 exec s
     | length s /= 1 = Nothing
     | otherwise = evalExpr $ head s
+
+exitWithHelp :: IO ()
+exitWithHelp = do
+    putStrLn "Usage: ./funEvalExpr 'your_expression'"
+    putStrLn "And please, a valid one"
+    exitWith (ExitFailure 84)
